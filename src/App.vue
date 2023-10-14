17e41f5d5d410ce5c09daa5fb4ed205b289ecaf5
dkilav <vlad5222vd@gmail.com>
2023-10-14 10:33:33 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+ 
+</script>
+
+<template>
+   <div>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facilis beatae quisquam tenetur autem nemo. Distinctio incidunt voluptatibus sapiente, itaque sunt totam expedita minus ad dolorem numquam, molestias labore suscipit dolor quae est, error voluptas. Consequatur reiciendis quibusdam saepe quis sequi eum velit consequuntur!</p>
+   </div>
+  
+</template>
+
+<style scoped>
+ p{
+  color: blue;
+  font-size: 33px;
+ }
+</style>
